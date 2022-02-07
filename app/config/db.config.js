d63ae375c428e6aa1bd33a8aea2e72b49beb9109
abdelmounaim-bgz <abdelmounaim.bougaze@gmail.com>
2022-02-07 17:35:54 +0100

    fix error

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,8 +2,7 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "ABc@2021",
-    DB: "Users",
-    PORT: 3306
+    DB: "Users"    
 };
 // module.exports = {
 //     HOST: "localhost",
